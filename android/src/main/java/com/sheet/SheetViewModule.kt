5486e0bf5c20c1a9db26a9d4965ead967d7e0399
SergeyMild <sergeymild@yandex.ru>
2023-06-14 06:51:12 +0300

    fix crash on android

--- android/src/main/java/com/sheet/SheetViewModule.kt
+++ android/src/main/java/com/sheet/SheetViewModule.kt
@@ -19,18 +19,25 @@ class SheetViewModule(reactContext: ReactApplicationContext) : ReactContextBaseJ
     return TAG
   }
 
-  private fun findSheetView(viewId: Int): AppFittedSheet {
-    Log.d(TAG, "Finding view $viewId...")
-    val view = if (reactApplicationContext != null) UIManagerHelper.getUIManager(reactApplicationContext, viewId)?.resolveView(viewId) as AppFittedSheet? else null
-    Log.d(TAG,  if (reactApplicationContext != null) "Found view $viewId!" else "Couldn't find view $viewId!")
-    return view ?: throw RuntimeException("ViewNotFound($viewId)")
+  private fun findSheetView(viewId: Int): AppFittedSheet? {
+    try {
+      Log.d(TAG, "Finding view $viewId...")
+      val view = if (reactApplicationContext != null) UIManagerHelper.getUIManager(reactApplicationContext, viewId)?.resolveView(viewId) as AppFittedSheet? else null
+      Log.d(TAG,  if (reactApplicationContext != null) "Found view $viewId!" else "Couldn't find view $viewId!")
+      return view ?: throw RuntimeException("ViewNotFound($viewId)")
+    } catch (e: Throwable) {
+      if (BuildConfig.DEBUG) {
+        e.printStackTrace()
+      }
+    }
+    return null
   }
 
   @ReactMethod
   fun dismiss(viewTag: Int) {
     Handler(Looper.getMainLooper()).post {
       val view = findSheetView(viewTag)
-      view.dismiss()
+      view?.dismiss()
     }
   }
 }
