976a278900b15d0c207b066fa479fab1332bd348
Sergei Golishnikov <sergeymild@yandex.ru>
2024-06-08 12:10:18 +0300

    cache dialog window

--- android/src/main/java/com/sheet/FragmentModalBottomSheet.kt
+++ android/src/main/java/com/sheet/FragmentModalBottomSheet.kt
@@ -7,7 +7,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.lang.ref.WeakReference
 
 class FragmentModalBottomSheet(
   private val modalView: ViewGroup,
@@ -15,6 +17,10 @@ class FragmentModalBottomSheet(
   private val isDark: Boolean,
   private val onDismiss: () -> Unit
 ) : BottomSheetDialogFragment() {
+
+  companion object {
+    var presentedWindow: WeakReference<Window>? = null
+  }
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -26,11 +32,14 @@ class FragmentModalBottomSheet(
     this.isCancelable = dismissible
     val dialog = CustomBottomSheetDialog(requireContext(), if (isDark) R.style.AppBottomSheetDialog_Dark else R.style.AppBottomSheetDialog)
     dialog.setSheetBackgroundColor(Color.TRANSPARENT)
+    dialog.window?.let {presentedWindow = WeakReference(it)}
     return dialog
   }
 
   override fun onDismiss(dialog: DialogInterface) {
     super.onDismiss(dialog)
+    presentedWindow?.clear()
+    presentedWindow = null
     onDismiss()
   }
 }
