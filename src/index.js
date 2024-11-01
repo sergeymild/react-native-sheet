94bfdf55519f8a2883670ceb25223af11cafd6f8
Sergei Golishnikov <sergeymild@yandex.ru>
2024-11-01 16:41:04 +0200

    remove js

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import { FITTED_SHEET_SCROLL_VIEW, FittedSheet, useFittedSheetContext, } from './FittedSheet';
-export { FittedSheet, FITTED_SHEET_SCROLL_VIEW, useFittedSheetContext };
-export { TopModal } from './TopModal';
