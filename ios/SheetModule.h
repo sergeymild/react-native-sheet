3ba6a288fd70973b5513822af86f7201ca0cbf21
SergeyMild <sergeymild@yandex.ru>
2025-07-08 16:01:44 +0300

    fix imports in ios

--- ios/SheetModule.h
+++ ios/SheetModule.h
@@ -1,5 +1,9 @@
 #import <Sheet/RNSheetViewSpec.h>
+#if __has_include("Sheet-Swift.h")
+#import "Sheet-Swift.h"
+#else
 #import <Sheet/Sheet-Swift.h>
+#endif
 
 @interface SheetModule : NSObject <NativeSheetSpec>
 
